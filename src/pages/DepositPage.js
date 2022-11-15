e450b59ad596fb14f138b9e065a78c71855c170f
erickssguerra <erickssguerra@gmail.com>
2022-11-15 18:05:47 -0300

    feat: building form in DepositPage

--- src/pages/DepositPage.js
+++ src/pages/DepositPage.js
@@ -1,21 +1,40 @@
 import TransactionPageStyle from "../assets/TransactionPageStyle"
 import FormStyle from "../assets/FormStyle"
+import { useState } from "react"
 
 export default function DepositPage() {
+
+    const [form, setForm] = useState({ price: "", description: "" })
+
+    function inputControl(event) {
+        setForm({
+            ...form, [event.target.name]: event.target.value
+        })
+    }
+
+    function deposit(event) {
+        event.preventDefault()
+        console.log(form)
+    }
+
     return (
         <TransactionPageStyle>
             <h1>Nova entrada</h1>
-            <FormStyle>
+            <FormStyle onSubmit={deposit}>
                 <input
                     type="text"
                     placeholder="Valor"
                     name="price"
+                    onChange={inputControl}
+                    value={form.price}
                     required
                 />
                 <input
                     type="text"
                     placeholder="Descrição"
                     name="description"
+                    onChange={inputControl}
+                    value={form.description}
                     required
                 />
                 <button>
